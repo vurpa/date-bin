d9f66e5f98b1f87c7a660e4426115762b867c20b
john <js@thesc.se>
2021-10-12 14:53:06 +0200

    first commit

---
+++ src/dateBin.spec.ts
@@ -0,0 +1,15 @@
+import dateBin from './dateBin'
+
+test('bin hour', () => {
+  // 1 hour in ms
+  const interval = 3_600_000;
+
+  const origin = new Date('2021-01-01T00:00:00')
+
+  expect(
+    dateBin(interval, new Date('2021-01-01T08:30:00'), origin).getTime()
+  ).toBe(new Date('2021-01-01T08:00:00').getTime())
+  expect(
+    dateBin(interval, new Date('2021-01-01T08:00:00'), origin).getTime()
+  ).toBe(new Date('2021-01-01T08:00:00').getTime())
+})
---
+++ src/dateBin.ts
@@ -0,0 +1,17 @@
+function dateBin(interval: number, source: Date, origin: Date): Date {
+  const sourceTime = source.getTime()
+  const originTime = origin.getTime()
+
+  const diff = sourceTime - originTime
+
+  let delta = diff - (diff % interval);
+
+  if (originTime > sourceTime)
+		delta -= interval;
+
+  const result = originTime + delta
+
+  return new Date(result)
+}
+
+export default dateBin
